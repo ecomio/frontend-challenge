9430e0f125a208d62553bfa58a71f31a250a4a38
Hamid Alizadeh <alizadeh@rhrk.uni-kl.de>
2022-05-01 03:43:51 +0200

    feat(dom): add budget to beat button to contentscript

--- public/contentscript.js
+++ public/contentscript.js
@@ -1,11 +1,51 @@
-// console.log('Contentscript injected');
+const budgetBtnStyle = {
+  'background-color': 'darkblue',
+  color: 'white',
+  cursor: 'pointer',
+  'border-radius': '3px 5px 0 3px',
+  display: 'inline-flex',
+  'justify-content': 'space-evenly',
+  'align-items': 'center',
+  'align-content': 'center',
+  padding: '10px 15px',
+  gap: '10px',
+  float: 'right',
+  'font-size': '0.8rem',
+};
 
-// let element = document.createElement('div');
-// const injectedText = document.createTextNode(`Extension-like element`);
+const parentElement = document.querySelector(
+  '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > div:nth-child(2) > h2',
+);
 
-// element.appendChild(injectedText);
+const budgetValue = document
+  .querySelector(
+    '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > div:nth-child(3) > p:nth-child(3)',
+  )
+  .textContent.trim();
 
-// const parentElement = document.querySelector(
-//   '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > div:nth-child(2) > h2',
-// );
-// parentElement.appendChild(element);
+const budgetBtn = document.createElement('div');
+budgetBtn.classList.add('budget-to-beat');
+cssStyler(budgetBtn, budgetBtnStyle);
+
+const budgetImg = document.createElement('img');
+budgetImg.src = './images/favicon-16x16.png';
+budgetImg.style.width = '16px';
+
+const budgetText = document.createElement('span');
+budgetText.textContent = ` Budget-to-Beat: ${budgetValue}`;
+
+budgetBtn.appendChild(budgetImg);
+budgetBtn.appendChild(budgetText);
+parentElement.appendChild(budgetBtn);
+
+function cssStyler(element, style) {
+  for (const property in style) element.style[property] = style[property];
+}
+
+budgetBtn.addEventListener('mouseenter', () => {
+  budgetBtn.style.backgroundColor = 'blue';
+});
+
+budgetBtn.addEventListener('mouseleave', () => {
+  budgetBtn.style.backgroundColor = 'darkblue';
+});
